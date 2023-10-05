1436f93ddb28408fe94bf844bba3458175ac4bc4
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2023-10-05 14:38:50 +0530

    first commit

---
+++ blog/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <HelloWorld msg="NAMASTE VUE"/>
+    <childBro/>
+    <experimentNew/>
+  </div>
+</template>
+
+<script>
+import childBro from './components/childBro.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import experimentNew from './components/experimentNew.vue'
+export default {
+  name: 'App',
+  components: {
+    childBro,
+    HelloWorld,
+    experimentNew
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ blog/src/components/childBro.vue
@@ -0,0 +1,66 @@
+<template>
+    <h1>SIMPLE FORM</h1>
+    <ul>
+        <li v-for="item in error" v-bind:key="item">
+            {{ item }}: not filled
+        </li>
+    </ul>
+    <form>
+        <label>email</label>
+        <input type="text" placeholder="enter your email" v-model="form.email">
+        <br>
+        <label>password</label>
+        <input type="password" placeholder="enter password" v-model="form.password">
+        <br>
+        <button v-on:click="login" type="button">login</button>
+        <br>
+        <h1>Country</h1>
+        <select v-model="form.country">
+            <option>india</option>
+            <option>pak</option>
+            <option>china</option>
+        </select>
+        <br>
+        <h2>Technology</h2>
+        <label>java</label>
+        <input type="checkbox" value="java" v-model="form.technology">
+        <label>python</label>
+        <input type="checkbox" value="python" v-model="form.technology">
+        <br>
+        <h3>Gender</h3>
+        <label for="male">male</label>
+        <input type="radio" value="male" name="gender" v-model="form.gender">
+        <label for="female">female</label>
+        <input type="radio" value="female" name="gender" v-model="form.gender">
+
+    </form>
+</template>
+<script>
+export default {
+    name: 'childBro',
+    data() {
+        return {
+            form: {
+                email: '',
+                password: '',
+                gender: '',
+                technology: [],
+                country: ''
+            },
+            error: [],
+        }
+    },
+
+    methods: {
+        login() {
+            this.error=[];
+            for (const item in this.form) {
+                if (this.form[item] === '' || this.form[item].length === 0) {
+                    this.error.push(item)
+                }
+            }
+            console.warn(this.form, this.error)
+        }
+    }
+}
+</script>
