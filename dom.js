d3f46292c20ba9d83be6ace342e60507f931821b
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2023-10-07 23:51:03 +0530

    delete and Edit button

--- dom.js
+++ dom.js
@@ -1,22 +1,72 @@
-var listItem=document.querySelector('#items');
-listItem.parentElement.style.backgroundColor='lightgreen';
-listItem.lastElementChild.style.color='blue';
-listItem.lastChild.textContent='hello';
-listItem.children[0].style.color='magenta'; 
-console.log(listItem.firstElementChild);
-listItem.firstElementChild.textContent='firstEC';
-console.log(listItem.firstChild);
-console.log(listItem.nextSibling);
-console.log(listItem.nextElementSibling);
-console.log(listItem.previousSibling);
-console.log(listItem.previousElementSibling);
-listItem.previousElementSibling.style.fontWeight='bold';
-var newDiv=document.createElement('div');
-newDiv.className='hello';
-newDiv.id='hello1';
-newDiv.setAttribute('title','HelloDIV');
-var textNode=document.createTextNode('Hello world');
-newDiv.appendChild(textNode);
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
-container.insertBefore(newDiv,h1);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+  var editBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  editBtn.className = 'btn btn-edit btn-sm float-right edit';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+  editBtn.appendChild(document.createTextNode('E'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+  
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
