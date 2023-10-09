f8372009a0e5cf9d4904d0d530c4e7abf3f99f0d
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2023-10-09 16:10:12 +0530

    appointmentapp

--- dom.js
+++ dom.js
@@ -1,69 +1,11 @@
 var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
-
-// Form submit event
 form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
+function addItem(e) {
+    e.preventDefault();
 
-// Add item
-function addItem(e){
-  e.preventDefault();
-
-  // Get input value
-  var newItem = document.getElementById('item').value;
-  var kame= document.getElementById('iteml').value;
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
-  li.appendChild(document.createTextNode(kame));
-
-
-  // Create del button element
-  var deleteBtn = document.createElement('button');
-  var editBtn = document.createElement('button');
-
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-  editBtn.className = 'btn btn-edit btn-sm float-right edit';
-
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
-  editBtn.appendChild(document.createTextNode('E'));
-
-  // Append button to li
-  li.appendChild(deleteBtn);
-  li.appendChild(editBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
-
-}
-
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-   
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-  }
-}
-
-function filterItems(e){
-  var text = e.target.value.toLowerCase();
-  var items = itemList.getElementsByTagName('li');
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
+    // Get input value
+    var newIitem=document.getElementById('email').value;
+    var newItem=document.getElementById('name').value;
+    localStorage.setItem('email',newIitem);
+    localStorage.setItem('name',newItem);
 }
