28265e7e9db75df7f6f2bd73263d75bf1442ac32
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2023-10-09 14:27:03 +0530

    search

--- dom.js
+++ dom.js
@@ -15,13 +15,15 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
-
+  var kame= document.getElementById('iteml').value;
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(kame));
+
 
   // Create del button element
   var deleteBtn = document.createElement('button');
@@ -41,26 +43,21 @@ function addItem(e){
 
   // Append li to list
   itemList.appendChild(li);
-  
+
 }
 
 // Remove item
 function removeItem(e){
   if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
+   
       var li = e.target.parentElement;
       itemList.removeChild(li);
-    }
   }
 }
 
-// Filter Items
 function filterItems(e){
-  // convert text to lowercase
   var text = e.target.value.toLowerCase();
-  // Get lis
   var items = itemList.getElementsByTagName('li');
-  // Convert to an array
   Array.from(items).forEach(function(item){
     var itemName = item.firstChild.textContent;
     if(itemName.toLowerCase().indexOf(text) != -1){
