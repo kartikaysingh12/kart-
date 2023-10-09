882739b7ab7a7adbb5a5230914fa1b24dfc0e131
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2023-10-09 22:59:56 +0530

    storing obj

--- dom.js
+++ dom.js
@@ -1,11 +1,7 @@
-var form = document.getElementById('addForm');
-form.addEventListener('submit', addItem);
-function addItem(e) {
-    e.preventDefault();
-
-    // Get input value
-    var newIitem=document.getElementById('email').value;
-    var newItem=document.getElementById('name').value;
-    localStorage.setItem('email',newIitem);
-    localStorage.setItem('name',newItem);
-}
+let myObj={
+    name:"kartikey",
+    age:25
+};
+let myObj_sort=JSON.stringify(myObj);
+console.log(myObj_sort);
+localStorage.setItem('myobj',myObj_sort);
