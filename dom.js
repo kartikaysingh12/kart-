d5dcade750a981514ee61a43c52b41aeff510010
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2023-10-10 15:30:29 +0530

    scale the app

--- dom.js
+++ dom.js
@@ -1,7 +1,33 @@
-let myObj={
-    name:"kartikey",
-    age:25
-};
-let myObj_sort=JSON.stringify(myObj);
-console.log(myObj_sort);
-localStorage.setItem('myobj',myObj_sort);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+form.addEventListener('submit', addItem);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('name').value;
+  var newIttem = document.getElementById('email').value;
+  var newItttem = document.getElementById('number').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newIttem));
+  li.appendChild(document.createTextNode(newItttem));
+
+  // Append li to list
+  itemList.appendChild(li);
+  let myObj={
+    name:newItem,
+    email:newIttem,
+    number:newItttem
+  };
+  let my_Obj=JSON.stringify(myObj);
+  localStorage.setItem('myObj',my_Obj);
+
+}
